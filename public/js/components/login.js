62401c5ed159dc6b65e7c2b85e15ffce505add4f
AC287 <arthurchen287@gmail.com>
2016-03-18 15:51:24 -0400

    login and logout success

--- public/js/components/login.js
+++ public/js/components/login.js
@@ -1,17 +1,38 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Firebase from 'firebase'
+
+var ref = new Firebase('https://crackling-torch-879.firebaseio.com/');
 
 export default React.createClass({
 
-
+  handleSubmit: function(event) {
+    event.preventDefault();
+    ref.authWithPassword({
+      email: this.refs.email.value,
+      password: this.refs.password.value,
+    }, (error, authData) => {
+      if(error) {
+        console.log('Login Failed.', error);
+      } else {
+        console.log('Login success: ', authData.uid)
+      }
+    })
+    this.refs.loginform.reset();
+  },
 
   render: function(){
 
     return(
       <div>
-      <div><Link to="/">Home</Link></div>
-        <div>Login component</div>
-
+        <div><Link to="/">Home</Link></div>
+        <div>
+          <form ref="loginform" onSubmit={this.handleSubmit}>
+            <input ref="email" placeholder="email" /><br/>
+            <input ref="password" placeholder="password" type="password" /><br/>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     )
   }
--- public/js/components/logout.js
+++ public/js/components/logout.js
@@ -1,17 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Firebase from 'firebase'
+
+var ref = new Firebase('https://crackling-torch-879.firebaseio.com/');
+
 
 export default React.createClass({
 
-
+  handleClick: function(event) {
+    event.preventDefault();
+    if(ref.getAuth()){
+      ref.unauth();
+      console.log('Successfully logged out');
+    } else {
+      console.log('You need to log in to log out');
+    }
+  },
 
   render: function(){
 
+    if(!ref.getAuth()){
+      console.log('You need to login');
+    } else {
+      console.log('Welcome ' + ref.getAuth().uid);
+    }
+
+
     return(
       <div>
-      <div><Link to="/">Home</Link></div>
-        <div>Logout component</div>
-
+        <div><Link to="/">Home</Link></div>
+        <div>
+          <button ref="logout" onClick={this.handleClick}>Logout </button>
+        </div>
       </div>
     )
   }
