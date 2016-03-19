5e47c0b93901293aa0aed151cbfcd8288f21b7d7
Karolin Rafalski <kr2174@caa.columbia.edu>
2016-03-19 13:01:44 -0400

    nav bar functioning with landing page, plus a little css

--- public/js/components/create_new_template.js
+++ public/js/components/create_new_template.js
@@ -12,6 +12,8 @@ export default React.createClass({
       <div><Link to="/">Home</Link></div>
         <div>
         This is where a user can create a template
+        <p></p>
+        <div><Link to="/form">Form</Link></div>
         </div>
 
       </div>
--- public/js/components/picktemplate.js
+++ public/js/components/picktemplate.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Form from './form.js'
+
 
 export default React.createClass({
 
@@ -12,7 +14,9 @@ export default React.createClass({
       <div><Link to="/">Home</Link></div>
         <div>View of all titles of templates that can be clicked on to go to a play form to fill out nouns and verbs</div>
         <div><Link to="/form">Form</Link></div>
-      <div> {this.props.children} </div>
+        <Form></Form>
+      <div> </div>
+      
       </div>
     )
   }
