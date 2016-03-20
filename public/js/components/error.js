813bc27cff27b50fdc440f3e132a2e2433fba21e
AC287 <arthurchen287@gmail.com>
2016-03-20 17:37:33 -0400

    random word is now generating for 404 page.

--- public/js/components/error.js
+++ public/js/components/error.js
@@ -2,27 +2,28 @@
 import React from 'react'
 import $ from 'jquery'
 
-var randomWord = 'http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+var randomWord = 'http://api.wordnik.com:80/v4/words.json/randomWord?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 
 export default React.createClass({
 
-  // getInitialState: function(){
-  //   return {
-  //     rword: {}
-  //   }
-  // },
-  //
-  // componentDidMount: function(){
-  //   console.log(process.env.API)
-  //   $.get(randomWord).done( data=> {
-  //     console.log(data);
-  //   })
-  // },
+  getInitialState: function(){
+    return {
+      rword: '',
+    }
+  },
+
+  componentDidMount: function(){
+    console.log(process.env.API)
+    $.get(randomWord).done( data=> {
+      this.setState({rword: data.word})
+    })
+  },
 
   render: function() {
     return(
       <div>
-        <h1> 404 not found </h1>
+        <h1>This page doesn't exist. Here is a random word for your effort.</h1>
+        <h1>{this.state.rword}</h1>
       </div>
     )
   }
