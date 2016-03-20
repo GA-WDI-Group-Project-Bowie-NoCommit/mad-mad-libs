ff7ac0c4a2df23fca7df2ada6689fc6ff2cb7d81
Caleb Sundance <calebrechten@gmail.com>
2016-03-20 15:45:57 -0400

    have form fields rendering dynamically, depending on the madlib template

--- public/js/components/stories_new.js
+++ public/js/components/stories_new.js
@@ -17,7 +17,9 @@ export default React.createClass({
 
     this.firebaseRef = new Firebase("https://amber-heat-1866.firebaseio.com/");
 
-    this.firebaseRef.on("child_added", function(dataSnapshot) {
+    var title = this.props.params.id
+
+    this.firebaseRef.orderByChild("title").equalTo(title).on("child_added", function(dataSnapshot) {
 
       this.state.template.push(dataSnapshot.val());
 
@@ -33,6 +35,44 @@ export default React.createClass({
 
   },
 
+  renderTitle: function(){
+
+  var template = this.state.template[0].title;
+
+    return(
+      <div>
+        {template}
+      </div>
+    )
+  },
+
+  renderTemplate: function(){
+
+  var thing = []
+  var storyText = this.state.template[0].text;
+  var nouns = 0;
+  var verbs = 0;
+
+  var textArray = storyText.split(' ').forEach(function(element, index){
+
+    if(element === "_noun_"){
+      nouns++
+      thing.push(<input ref={`noun${nouns}`} type="text" placeholder="Noun" />)
+    } else if (element === "_verb_"){
+      verbs++
+      thing.push(<input ref={`verb${verbs}`} type="text" placeholder="Verb" />)
+    }
+
+  })
+
+  return(
+    <div>
+      {thing}
+    </div>
+  )
+
+  },
+
   render: function(){
 
     var style = {
@@ -44,11 +84,10 @@ export default React.createClass({
       <div>
         <div>this is where you fill out the nouns and verbs with just knowing the title of the story</div>
 
+        {this.renderTitle()}
           <div className="form" style={style}>
             <form ref="storyForm" onSubmit={this.handleSubmit}>
-              <input ref="noun1" type="text" placeholder="Noun" />
-              <input ref="verb2" type="text" placeholder="Verb" />
-              <textarea ref="test" rows="5"> Testing what is here can we put something in dynamically for editing later? </textarea>
+              {this.renderTemplate()}
               <button type="submit">Form Button</button>
             </form>
           </div>
