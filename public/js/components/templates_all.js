553cf3cc19317f0d79fbfcb2fdff3a859f8e3c4c
Umbrellagun <Umbrellagun@users.noreply.github.com>
2016-03-21 16:23:25 -0400

    Update templates_all.js

--- public/js/components/templates_all.js
+++ public/js/components/templates_all.js
@@ -37,7 +37,7 @@ export default React.createClass({
     render: function(){
       return(
         <div>
-          Make your own version of a story!
+          Fill out a new Mad Lib!
           <AllTemplates templates={Object.keys(this.state.templates).map(this.renderTemplates)} />
           {this.props.children}
         </div>
